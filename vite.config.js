f3adf04aa11a78640451d0137798c496c705445a
zag <za12ew44zz_999@hotmail.com>
2025-03-02 11:58:49 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({ 
   plugins: [react()],
-  base: '/zag5a6167/',
-});
+  base: mode === 'production' ? '/Movie-Cinema-Seat/' : '/', 
+  build: {
+    outDir: 'dist'
+  }
+}));
